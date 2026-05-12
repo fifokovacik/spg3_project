d1c7d4aaf2df9a6b6a55c3032b618a2bbc5e1642
fifokovacik <felipe.kovacik@gmail.com>
2026-05-12 08:35:17 +0200

    trying out things in tkinter

--- app.py
+++ app.py
@@ -9,7 +9,21 @@ from min_heap import BinaryMinHeap
 root = tkinter.Tk()
 root.title("Data structure visualisation")
 root.geometry("1200x600")
+root2 = tkinter.Tk()
+root2.title("Data structure visualisation")
 
+def try1():
+    try:
+        root2.destroy()
+    except:
+        return
 
+#600x600 canvas -> 300-900, 0-600
+#0-300, 0-600 - data structure type buttons, load and save entries and buttons
+#900-1200, 0-600 - data structure specialized functions(buttons, entries; deleted after change of data structure)
+c=tkinter.Canvas(root,width=600,height=600, bg="white")
+c.place(x=300)
+try_button = tkinter.Button(root,text="Enter",command=try1)
+try_button.place(x=10)
 
 root.mainloop()
